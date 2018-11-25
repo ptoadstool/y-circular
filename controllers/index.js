8a73216b566726629e2b04210ce8d6a54c3cb4c9
Bella Steiner <bella.steiner@yale.edu>
2018-11-25 04:53:13 +0000

    finished homepage

--- controllers/index.js
+++ controllers/index.js
@@ -3,22 +3,31 @@ const eventModels = require('../models/events.js');
 // Create a function which is a "controller", it
 // handles a request, writing the response.
 function index(request, response) {
-    /* Creates array of event info to display */
-    const allEvents = eventModels.all;
-    const display = [];
-    // https://stackoverflow.com/questions/7196212/how-to-create-dictionary-and-add-key-value-pairs-dynamically
-    for (let i = 0; i < allEvents.length; i += 1) {
-        display[allEvents[i].title] = allEvents[i].date;
-        // display[allEvents[i].date] = allEvents[i].title;
-    }
     const contextData = {
-        title: 'All event',
+        title: 'event',
         salutation: 'Hello Event Makers, Goers, and Funders!',
         header: 'All Events',
-        display,
+        eventData: [],
     };
-    console.log(display);
-    response.render('index', contextData);
+    /* Creates array of event info to display */
+    const allEvents = eventModels.all;
+    // const display = [];
+    // https://stackoverflow.com/questions/7196212/how-to-create-dictionary-and-add-key-value-pairs-dynamically
+    const eventData = [];
+    for (let i = 0; i < allEvents.length; i += 1) {
+        const event = {};
+        event.id = allEvents[i].id;
+        event.title = allEvents[i].title;
+        event.date = allEvents[i].date;
+        // contextData[`title${j}`] = allEvents[i].title;
+        // contextData[`date${j}`] = allEvents[i].date;
+        // j += 1;
+        // console.log(event);
+        eventData.push(event);
+    }
+    contextData.eventData = eventData;
+    console.log(contextData);
+    return response.render('index', contextData);
 }
 
 module.exports = {
