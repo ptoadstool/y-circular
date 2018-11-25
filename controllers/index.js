57e0c04208cac2a6fe3f03016cedef2124699947
Bella Steiner <bella.steiner@yale.edu>
2018-11-25 01:32:58 +0000

    WIP: get evetns on homepage dynamically

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,23 @@
+const eventModels = require('../models/events.js');
+
 // Create a function which is a "controller", it
 // handles a request, writing the response.
 function index(request, response) {
+    /* Creates array of event info to display */
+    const allEvents = eventModels.all;
+    const display = [];
+    // https://stackoverflow.com/questions/7196212/how-to-create-dictionary-and-add-key-value-pairs-dynamically
+    for (let i = 0; i < allEvents.length; i += 1) {
+        display[allEvents[i].title] = allEvents[i].date;
+        // display[allEvents[i].date] = allEvents[i].title;
+    }
     const contextData = {
-        title: 'All Events',
+        title: 'All event',
         salutation: 'Hello Event Makers, Goers, and Funders!',
         header: 'All Events',
-
+        display,
     };
+    console.log(display);
     response.render('index', contextData);
 }
 
